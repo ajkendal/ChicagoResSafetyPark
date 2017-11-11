d692289d76a321334c1a68431d5a7fbf6091bab8
Amanda J Kendal-Brown <ajkendal@neiu.edu>
2017-11-11 16:22:52 -0600

    unfinished javascipt

---
+++ js/crime_data.js
@@ -0,0 +1,23 @@
+//https://data.cityofchicago.org/resource/6zsd-86xi.json
+var requestURL = "https://data.cityofchicago.org/resource/6zsd-86xi.json";
+var request = new XMLHttpRequest();
+var primaryType = "CRIMINAL DAMAGE";
+var description = "TO VEHICLE";
+
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+request.onload = function() {
+    "use strict";
+    var crimeData = request.response;
+    
+    var selections = crimeData.filter(function (crimes) {
+        return(crimes.primary_type === primaryType && crimes.description === description);
+    });
+    
+    for (var x = 0; x < selections.length; x++){
+        document.writeln(selections[x].latitude + " " + selections[x].longitude);
+    }
+    
+}
