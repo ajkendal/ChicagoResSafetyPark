0f70a0924edff45c988be28b51c1c3ebfbd8af5d
Bahar radfar <br.radfar@gmail.com>
2017-11-12 03:19:29 -0600

    change the test.py

--- www/test.py
+++ www/test.py
@@ -1,1 +1,6 @@
-print("Helloo Bahar")
+# print("Helloo Bahar")
+def main():
+    return "Hi Saeed :)"
+if __name__ == '__main__':
+    main()
+print("--- %s seconds ---" % (time.time() - start_time))
