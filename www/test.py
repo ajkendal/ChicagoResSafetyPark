4151e42769d8f20acccb7fe60f5c65e776583646
Bahar radfar <br.radfar@gmail.com>
2017-11-12 03:33:45 -0600

    add helper file

--- www/test.py
+++ www/test.py
@@ -1,6 +1,10 @@
 # print("Helloo Bahar")
 def main():
+    f = open('wards_codes.txt','w')
+    f.write('Hello World')
+    f.close()
     return "Hi Saeed :)"
+
 if __name__ == '__main__':
     main()
-print("--- %s seconds ---" % (time.time() - start_time))
+# print("--- %s seconds ---" % (time.time() - start_time))
