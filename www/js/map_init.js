a3dd0e1da513940be5ee7d2118ba91cb08b899e0
Amanda J Kendal-Brown <ajkendal@neiu.edu>
2017-11-12 11:23:20 -0600

    changes

--- www/js/map_init.js
+++ www/js/map_init.js
@@ -1,4 +1,5 @@
 var map;
+
 function initMap(userLocation, userDate, userTime) {
     var latLng = {lat: userLocation.lat, lng: userLocation.lng};
     var starIcon = "/ChicagoResSafetyPark/img/small_star.png";
@@ -29,39 +30,19 @@ function initMap(userLocation, userDate, userTime) {
 
     var markerCluster = new MarkerClusterer(map, markers,options);
     
-    var apiCustomUrl = "https://cors.io/?https://mellodi.pythonanywhere.com/setBucket";
+    
+    /*var apiCustomUrl = "https://cors.io/?https://mellodi.pythonanywhere.com/setBucket";
     var apiRequest = new XMLHttpRequest();
-    //apiRequest.open('GET', apiCustomUrl, true);
-    //apiRequest.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+    apiRequest.open('GET', requestURL);
     
     var obj = { "lat": userLocation.lat, "lng": userLocation.lng, "date": userDate, "time": userTime};
     
     var newJson = JSON.stringify(obj);
     
     console.log(newJson);
-    
-    //apiRequest.send(newJson);
-    
-    
-    
-    //$.post(apiCustomUrl, newJson);
-    
-   /* console.log(newJson);
 
-    $.post(apiCustomUrl, function(data, ){
-        console.log(userLocation, userDate, userTime);
-        
-    }).done(function(data) {
-        console.log(data);
-    });*/
     
-    $.ajax({
-        url: apiCustomUrl,
-        type: 'post',
-        data: newJson,
-        headers: {
-            'Content-type': 'application/json',
-            'Accept': 'text/plain'
-        }
-    })
+    $.post(apiCustomUrl, {
+        javascript_data: newJson
+    });*/
 }
