89b1fc381a5239023e67bca2a724b48356dab871
Bahar radfar <br.radfar@gmail.com>
2017-11-12 04:55:56 -0600

    add helper file

--- www/test.py
+++ www/test.py
@@ -1,10 +1,30 @@
 # print("Helloo Bahar")
-def main():
+import time   #Importing the time library to check the time of code execution
+import sys    #Importing the System Library
+import os
+import socket
+
+from flask import Flask
+from flask import render_template
+from flask import jsonify
+app = Flask(__name__)
+# def main():
+#     f = open('wards_codes.js','w')
+#     f.write('console.log(Hello World);')
+#     f.close()
+#     return "Hi Saeed :)"
+
+@app.route('/test')
+def server():
     f = open('wards_codes.js','w')
     f.write('console.log(Hello World);')
     f.close()
-    return "Hi Saeed :)"
+    return render_template('index.html')
 
-if __name__ == '__main__':
-    main()
+
+if __name__ == "__main__":
+  app.run()
+
+# if __name__ == '__main__':
+#     main()
 # print("--- %s seconds ---" % (time.time() - start_time))
