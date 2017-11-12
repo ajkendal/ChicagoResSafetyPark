371036a8dfce4f678a1e89c80c65ea1a1767f1bd
Bahar radfar <br.radfar@gmail.com>
2017-11-12 07:21:36 -0600

    another one

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ def hello():
     return 'Hello World!'
 
 
-@app.errorhandler(500)
-def server_error(e):
-    logging.exception('An error occurred during a request.')
-    return """
-        An internal error occurred: <pre>{}</pre>
-        See logs for full stacktrace.
-        """.format(e), 500
+#@app.errorhandler(500)
+#def server_error(e):
+#   logging.exception('An error occurred during a request.')
+#   return """
+#       An internal error occurred: <pre>{}</pre>
+#       See logs for full stacktrace.
+#       """.format(e), 500
 
 
 if __name__ == '__main__':
